6a5bcff54e831491398d425074a982770890cafe
Erlend Aakre <Erlend.Aakre@camcog.com>
2015-06-09 15:11:03 +0100

    added bad unit test.. for testing CI notification

--- src/test/java/org/aakretech/istgame/logic/GameTest.java
+++ src/test/java/org/aakretech/istgame/logic/GameTest.java
@@ -1,7 +1,14 @@
 package org.aakretech.istgame.logic;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GameTest {
-    // TODO
+
+    @Test
+    public void testStuff() {
+        Assert.assertEquals(5, 10);
+    }
 }
